227676eb15ce700b531e5d9aaa71e6c50f532499
Hook <knightw23@163.com>
2018-11-23 18:19:41 +0800

    Java 多线程笔记与代码

--- Concurrency/concurrency/src/main/java/com/rock/atomic/AtomicExample5.java
+++ Concurrency/concurrency/src/main/java/com/rock/atomic/AtomicExample5.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
+import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 @Slf4j
 @ThreadSafe
