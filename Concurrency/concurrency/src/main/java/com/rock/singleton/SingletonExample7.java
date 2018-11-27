b27df421c29befd8c6c6075946479f08ea377f0a
Hook <knightw23@163.com>
2018-11-27 19:09:57 +0800

    安全发布对象

---
+++ Concurrency/concurrency/src/main/java/com/rock/singleton/SingletonExample7.java
@@ -0,0 +1,40 @@
+package com.rock.singleton;
+
+import com.rock.annoations.Recommend;
+import com.rock.annoations.ThreadSafe;
+
+
+/**
+ * 枚举模式，最安全的
+ * 推荐使用
+ */
+@ThreadSafe
+@Recommend
+public class SingletonExample7 {
+
+    /**
+     * 私有构造方法
+     */
+    private SingletonExample7() {
+
+    }
+
+    public static SingletonExample7 getSingletonExample7() {
+        return Singleton.INSTANCE.getInstance();
+    }
+
+    private enum Singleton {
+        INSTANCE;
+
+        private SingletonExample7 instance;
+
+        //JVM保证这个方法只被调用一次
+        Singleton() {
+            instance = new SingletonExample7();
+        }
+
+        public SingletonExample7 getInstance() {
+            return instance;
+        }
+    }
+}
--- Concurrency/concurrency/src/main/java/com/rock/sync/SyncExample.java
+++ Concurrency/concurrency/src/main/java/com/rock/sync/SyncExample.java
@@ -2,9 +2,6 @@ package com.rock.sync;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
 @Slf4j
 public class SyncExample {
 
